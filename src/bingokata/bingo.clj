744cc3208d1bbf3d4a9e60cdfe00781f62f7e40c
Angel Rojo Marijuan <arojomarijuan@magento.com>
2018-02-13 22:02:56 +0100

    end of session 5

--- src/bingokata/bingo.clj
+++ src/bingokata/bingo.clj
@@ -1,4 +1,5 @@
-(ns bingokata.bingo)
+(ns bingokata.bingo
+  (:require [bingokata.bingo-card :as card]))
 
 (defn call-number [{:keys [available-balls]}]
   {:current (first available-balls)
@@ -11,4 +12,17 @@
 (defn turn [game]
   (let [{:keys [current available-balls]} (call-number game)]
     (println current)
-    (assoc game :available-balls available-balls)))
+    (assoc game :available-balls available-balls :called-balls (conj (:called-balls game) current))))
+
+(defn winner? [game card]
+  (card/bingo? card (:called-balls game)))
+
+(defn end-of-game? [game]
+  (some (partial winner? game) (:players game)))
+
+(defn play [game]
+  (let [result (first (drop-while (complement end-of-game?)
+                                  (iterate turn game)))]
+    (println "BINGO!")
+    (println (map (partial winner? result) (:players result)))
+    result))
--- test/bingokata/game_test.clj
+++ test/bingokata/game_test.clj
@@ -16,10 +16,16 @@
                          :o [70 66 69 75 74]}]
               :available-balls [1]
               :called-balls []}]
-       (is (= "1\n" (with-out-str
-                      (is (= [] (:available-balls (bingo/turn game)))))))
-       (is (= "2\n" (with-out-str
-                      (is (= [3] (:available-balls (bingo/turn (assoc game :available-balls [2 3]))))))))))
-
-
+    (is (= "1\n" (with-out-str
+                   (is (= [] (:available-balls (bingo/turn game)))))))
+    (is (= "2\n" (with-out-str
+                   (is (= [3] (:available-balls (bingo/turn (assoc game :available-balls [2 3]))))))))
+    (is (= "2\n" (with-out-str
+                   (is (= [2] (:called-balls (bingo/turn (assoc game :available-balls [2 3]))))))))
+    (is (= "13\n11\n3\n10\n9\n26\n23\n24\n25\n27\n44\n33\n38\n41\n60\n58\n59\n56\n55\n68\n63\n73\n74\n71\n"
+           (with-out-str (is (= [] (:available-balls (first (drop 24 (iterate
+                                                                       bingo/turn
+                                                                       (assoc game :available-balls [13 11 3 10 9 26 23 24 25 27 44 33 38 41 60 58 59 56 55 68 63 73 74 71]))))))))))
+    (is (= "13\n11\n3\n10\n9\n26\n23\n24\n25\n27\n44\n33\n38\n41\n60\n58\n59\n56\n55\n68\n63\n73\n74\n71\nBINGO!\n(true false)\n"
+           (with-out-str (is (= [] (:available-balls (bingo/play (assoc game :available-balls [13 11 3 10 9 26 23 24 25 27 44 33 38 41 60 58 59 56 55 68 63 73 74 71]))))))))))
 
